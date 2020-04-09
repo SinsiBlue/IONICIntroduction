06b9b188f5d23433658f78c9ffc7c005c13fcbfb
Sinsi <sylvienumuhire@gmail.com>
2020-04-09 17:02:33 +0200

    tabs : intro générale

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   imports: [
